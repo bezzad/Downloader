ad539101685a90ea21b1f3fa53ff38158373ab80
Behzad <behzad.khosravifar@gmail.com>
2020-03-25 16:53:35 +0430

    ass chunk type to use instead range structer

---
+++ src/Downloader/DownloadService.Chunk.cs
@@ -0,0 +1,23 @@
+﻿namespace Downloader
+{
+    public partial class DownloadService
+    {
+        protected class Chunk
+        {
+            public Chunk(long start, long end)
+            {
+                Start = start;
+                End = end;
+                Position = 0;
+                Data = new byte[Length];
+            }
+
+            public long Id => Start.PairingFunction(End);
+            public long Start { get; set; }
+            public long End { get; set; }
+            public int Position { get; set; }
+            public long Length => End - Start + 1;
+            public byte[] Data { get; set; }
+        }
+    }
+}
