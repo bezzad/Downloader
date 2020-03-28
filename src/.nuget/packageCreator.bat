9b40fe6ec64434ef1443db08bb7b81184593528d
Behzad <behzad.khosravifar@gmail.com>
2020-03-28 16:20:33 +0430

    remove unused nuget package builder files

--- src/.nuget/packageCreator.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-Echo ---------------------- Nuget Packaging ---------------------------------
-
-mkdir ..\..\NuGetPackages 2> NUL
-nuget pack -OutputDirectory ..\..\NuGetPackages package.nuspec
-
-Echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-pause
