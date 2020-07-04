1e435fd0e2f6bb545f7f1e978330cbf300887e54
Behzad <behzad.khosravifar@gmail.com>
2020-07-04 14:55:49 +0430

    Added events to interface of DownloadService

--- src/Downloader/IDownloadService.cs
+++ src/Downloader/IDownloadService.cs
@@ -1,12 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace Downloader
 {
     public interface IDownloadService
     {
-        /// <summary>
-        /// Is in downloading time
-        /// </summary>
         bool IsBusy { get; }
         string MainProgressName { get; }
         long DownloadSpeed { get; set; }
@@ -16,5 +15,9 @@ namespace Downloader
         Task DownloadFileAsync(string address, string fileName);
         void CancelAsync();
         void Clear();
+
+        event EventHandler<AsyncCompletedEventArgs> DownloadFileCompleted;
+        event EventHandler<DownloadProgressChangedEventArgs> DownloadProgressChanged;
+        event EventHandler<DownloadProgressChangedEventArgs> ChunkDownloadProgressChanged;
     }
 }
