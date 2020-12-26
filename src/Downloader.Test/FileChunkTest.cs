5bd654af0965cbe960efd73b00a44d67faec61bc
behzad khosravifar <behzadkh@hotmail.com>
2020-12-26 18:13:20 +0330

    Added chunk class unit tests

---
+++ src/Downloader.Test/FileChunkTest.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Downloader.Test
+{
+    [TestClass]
+    public class FileChunkTest
+    {
+        [TestMethod]
+        public void ClearTest()
+        {
+            // arrange
+            var chunk = new FileChunk(0, 1000) {
+                FileName = Path.GetTempFileName(),
+                Position = 100,
+                Timeout = 100
+            };
+            chunk.CanTryAgainOnFailover();
+            File.Create(chunk.FileName).Close();
+
+            // act
+            chunk.Clear();
+
+            // assert
+            Assert.AreEqual(0, chunk.Position);
+            Assert.AreEqual(0, chunk.Timeout);
+            Assert.AreEqual(0, chunk.FailoverCount);
+            Assert.IsFalse(File.Exists(chunk.FileName));
+        }
+    }
+}
