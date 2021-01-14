4e68ef5eb2f542847b26a9a6cf4804cff224619b
Behzad <behzad.khosravifar@gmail.com>
2021-01-14 22:28:51 +0330

    Add missing members of DownloadService to self interface

--- src/Downloader/IDownloadService.cs
+++ src/Downloader/IDownloadService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Downloader
@@ -12,9 +13,11 @@ namespace Downloader
         event EventHandler<AsyncCompletedEventArgs> DownloadFileCompleted;
         event EventHandler<DownloadProgressChangedEventArgs> DownloadProgressChanged;
         event EventHandler<DownloadProgressChangedEventArgs> ChunkDownloadProgressChanged;
+        event EventHandler<DownloadStartedEventArgs> DownloadStarted;
 
         Task DownloadFileAsync(DownloadPackage package);
         Task DownloadFileAsync(string address, string fileName);
+        Task DownloadFileAsync(string address, DirectoryInfo folder);
         void CancelAsync();
         void Clear();
     }
