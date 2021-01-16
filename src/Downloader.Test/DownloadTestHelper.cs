dc3df866d10b1bdd2289b9a75fb423ad1d217a7c
Behzad <behzad.khosravifar@gmail.com>
2021-01-16 14:09:58 +0330

    Added asset files to resources of Test project to compare downloaded data with local files

--- src/Downloader.Test/DownloadTestHelper.cs
+++ src/Downloader.Test/DownloadTestHelper.cs
@@ -1,27 +1,27 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
+using Downloader.Test.Properties;
 
 namespace Downloader.Test
 {
     internal static class DownloadTestHelper
     {
-        public static string File1KbUrl { get; } =
-            "https://raw.githubusercontent.com/bezzad/Downloader/master/src/Downloader.Test/Assets/file_example_JSON_1kb.json";
+        public const string TempFilesExtension = ".temp";
+        public const string File1KbName = "file_example_JSON_1kb.json";
+        public const string File150KbName = "file-sample_150kB.pdf";
+        public const string File10MbName = "zip_10MB.zip";
+        public const int FileSize1Kb = 15547;
+        public const int FileSize150Kb = 142786;
+        public const int FileSize10Mb = 10679630;
 
-        public static string File150KbUrl { get; } =
-            "https://raw.githubusercontent.com/bezzad/Downloader/master/src/Downloader.Test/Assets/file-sample_150kB.pdf";
+        public static readonly string File1KbUrl = $"https://raw.githubusercontent.com/bezzad/Downloader/master/src/Downloader.Test/Assets/{File1KbName}";
+        public static readonly string File150KbUrl = $"https://raw.githubusercontent.com/bezzad/Downloader/master/src/Downloader.Test/Assets/{File150KbName}";
+        public static readonly string File10MbUrl = $"https://raw.githubusercontent.com/bezzad/Downloader/master/src/Downloader.Test/Assets/{File10MbName}";
+        public static readonly string TempDirectory = Path.GetTempPath();
 
-        public static string File10MbUrl { get; } =
-            "https://raw.githubusercontent.com/bezzad/Downloader/master/src/Downloader.Test/Assets/zip_10MB.zip";
-
-        public static string File1KbName { get; } = "file_example_JSON_1kb.json";
-        public static string File150KbName { get; } = "file-sample_150kB.pdf";
-        public static string TempDirectory { get; } = Path.GetTempPath();
-        public static string TempFilesExtension { get; } = ".temp";
-
-        public static int FileSize1Kb { get; } = 15547;
-        public static int FileSize150Kb { get; } = 142786;
-        public static int FileSize10Mb { get; } = 10679630;
+        public static byte[] File1Kb => Resources.file_example_JSON_1kb;
+        public static byte[] File150Kb => Resources.file_sample_150kB;
+        public static byte[] File10Mb => Resources.zip_10MB;
 
         public static async void CancelAfterDownloading(this IDownloadService ds, int delayMs)
         {
