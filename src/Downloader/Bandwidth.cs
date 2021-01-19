df40f0503023f88ab9597e975f1d2c7a7de081ee
Behzad <behzad.khosravifar@gmail.com>
2021-01-19 15:06:31 +0330

    Added bandwidth class to calculate download speed

---
+++ src/Downloader/Bandwidth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Downloader
+{
+    public class Bandwidth
+    {
+        private const int OneSecond = 1000; // millisecond
+        private long _count;
+        private int _lastTickCountCheckpoint;
+        public long Speed { get; private set; }
+        public long AverageSpeed { get; private set; }
+
+        public Bandwidth()
+        {
+            Reset();
+        }
+
+        public void CalculateSpeed(long bytesReceived)
+        {
+            int elapsedTime = Environment.TickCount - _lastTickCountCheckpoint + 1;
+            Speed = bytesReceived * OneSecond / elapsedTime; // bytes per second
+            AverageSpeed = ((AverageSpeed * _count) + Speed) / (_count + 1);
+            _lastTickCountCheckpoint = Environment.TickCount;
+            _count++;
+        }
+
+        public void Reset()
+        {
+            _lastTickCountCheckpoint = Environment.TickCount;
+            _count = 0;
+            Speed = 0;
+            AverageSpeed = 0;
+        }
+    }
+}
