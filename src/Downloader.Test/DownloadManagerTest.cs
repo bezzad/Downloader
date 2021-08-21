2bb9efe6c949ddaa9a6c00c73b0c0ba6362f58c7
Behzad <behzad.khosravifar@gmail.com>
2021-08-21 14:17:07 +0430

    Sign test project to use of InternalVisibleTo attribute

---
+++ src/Downloader.Test/DownloadManagerTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Downloader.Test
+{
+    [TestClass]
+    public class DownloadManagerTest
+    {
+        [TestInitialize]
+        public virtual void Initial()
+        {
+            var testData = DummyData.GenerateOrderedBytes(DownloadTestHelper.FileSize16Kb);
+            var x = new DownloadRequest(null, null);
+            
+        }
+    }
+}
