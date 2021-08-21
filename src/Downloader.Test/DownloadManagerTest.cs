6da41781334390ebb7e5a195dc88c24cc92c6d11
Behzad <behzad.khosravifar@gmail.com>
2021-08-21 21:50:03 +0430

    Added first version of TDD download manager

--- src/Downloader.Test/DownloadManagerTest.cs
+++ src/Downloader.Test/DownloadManagerTest.cs
@@ -1,16 +1,84 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Downloader.Test
 {
     [TestClass]
     public class DownloadManagerTest
     {
+        private long _mockFileTotalSize = 1024 * 100;
+        private IDownloadService _downloadService;
+
         [TestInitialize]
-        public virtual void Initial()
+        public void Initial()
         {
-            var testData = DummyData.GenerateOrderedBytes(DownloadTestHelper.FileSize16Kb);
-            var x = new DownloadRequest(null, null);
-            
+            var downloadServiceMock = new Mock<IDownloadService>(MockBehavior.Strict);
+            downloadServiceMock.SetupAllProperties();
+            downloadServiceMock.Setup(d => d.DownloadFileTaskAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((url, filename) => {
+                    downloadServiceMock.Object.Package = new DownloadPackage() {
+                        Address = url,
+                        FileName = filename,
+                        TotalFileSize = _mockFileTotalSize
+                    };
+
+                    // Start download...
+                    downloadServiceMock.Object.Package.IsSaving = true;
+                    downloadServiceMock.Raise(d => d.DownloadStarted+=null, new DownloadStartedEventArgs(url, _mockFileTotalSize));
+
+                    // Raise progress events
+                    var bytes = 1024;
+                    while (bytes < _mockFileTotalSize)
+                    {
+                        downloadServiceMock.Object.Package.SaveProgress = 100.0*bytes/_mockFileTotalSize;
+                        downloadServiceMock.Raise(d => d.DownloadProgressChanged+=null, new DownloadProgressChangedEventArgs(null) {
+                            BytesPerSecondSpeed = _mockFileTotalSize,
+                            AverageBytesPerSecondSpeed = _mockFileTotalSize,
+                            ProgressedByteSize = bytes,
+                            ReceivedBytesSize = 1024,
+                            TotalBytesToReceive = _mockFileTotalSize
+                        });
+                        bytes += 1024;
+                    }
+
+                    // Complete download
+                    downloadServiceMock.Object.Package.SaveProgress = 100.0*bytes/_mockFileTotalSize;
+                    downloadServiceMock.Object.Package.IsSaveComplete = true;
+                    downloadServiceMock.Object.Package.IsSaving = false;
+                    downloadServiceMock.Setup(d => d.CancelAsync()).Raises(d => d.DownloadFileCompleted+=null, new AsyncCompletedEventArgs(null, false, null));
+                })
+                .Returns(Task.Delay(100));
+
+            downloadServiceMock.Setup(d => d.IsBusy).Returns(true);
+            downloadServiceMock.Verify();
+            _downloadService = downloadServiceMock.Object;
+        }
+
+        [TestMethod]
+        public void DownloadTest()
+        {
+            _downloadService.DownloadStarted += _downloadService_DownloadStarted;
+            _downloadService.DownloadFileCompleted +=_downloadService_DownloadFileCompleted;
+            ;
+            _downloadService.DownloadFileTaskAsync("test", "test1").Wait();
+            _downloadService.DownloadFileTaskAsync("test1", "test1").Wait();
+            _downloadService.CancelAsync();
+            Assert.IsTrue(_downloadService.IsBusy);
+        }
+
+        private void _downloadService_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Debug.WriteLine(e.Error);
+            Debug.WriteLine(e.Cancelled);
+        }
+
+        private void _downloadService_DownloadStarted(object sender, DownloadStartedEventArgs e)
+        {
+            Debug.WriteLine(e.FileName);
         }
     }
 }
---
+++ src/Downloader/DownloadManager.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace Downloader
+{
+    public class DownloadManager : IDownloadManager
+    {
+        private Dictionary<string, DownloadRequest> _requests;
+        private int _numberOfDownloads;
+        public int NumberOfDownloads => _numberOfDownloads;
+        public int MaxNumberOfMultipleFileDownload { get; set; }
+        public DownloadConfiguration Configuration { get; }
+        public event EventHandler<IDownloadInfo> AddNewDownload;
+        public event EventHandler<IDownloadInfo> DownloadStarted;
+        public event EventHandler<IDownloadInfo> DownloadCompleted;
+        public event EventHandler<IDownloadInfo> DownloadProgressChanged;
+
+        public DownloadManager(DownloadConfiguration configuration, int maxNumberOfMultipleFileDownload)
+        {
+            Configuration = configuration;
+            MaxNumberOfMultipleFileDownload = maxNumberOfMultipleFileDownload;
+        }
+
+        public void DownloadAsync(params IDownloadInfo[] downloadInfos)
+        {
+            foreach (var download in downloadInfos)
+            {
+                DownloadAsync(download);
+            }
+        }
+        private void DownloadAsync(IDownloadInfo downloadInfo)
+        {
+            if (string.IsNullOrWhiteSpace(downloadInfo?.Url))
+                throw new ArgumentNullException(nameof(downloadInfo.Url));
+
+            var request = GetOrAddDownloadService(downloadInfo);
+        }
+
+        private DownloadRequest GetOrAddDownloadService(IDownloadInfo downloadInfo)
+        {
+            if (_requests.TryGetValue(downloadInfo.Url, out var request))
+                return request;
+
+            return CreateDownloadRequest(downloadInfo);
+        }
+        private DownloadRequest CreateDownloadRequest(IDownloadInfo downloadInfo)
+        {
+            var downloader = new DownloadService(Configuration);
+            downloader.DownloadFileCompleted += OnDownloadFileComplete;
+            var request = new DownloadRequest(downloadInfo, downloader);
+            _requests.Add(downloadInfo.Url, request);
+            OnAddNewDownload(downloadInfo);
+            return request;
+        }
+        private void RemoveRequest(DownloadRequest request)
+        {
+            lock (this)
+            {
+                Interlocked.Decrement(ref _numberOfDownloads);
+                Debug.Assert(_numberOfDownloads >= 0, "This algorithm isn't thread-safe! What do you do?");
+                request.DownloadInfo.IsSaving = false;
+            }
+        }
+
+        private void OnDownloadFileComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            if (sender is DownloadService downloader &&
+               _requests.TryGetValue(downloader.Package.Address, out var request))
+            {
+                RemoveRequest(request);
+                if (e.Cancelled == false)
+                {
+                    if (e.Error == null) // download completed
+                    {
+                        //onDownloadCompleted?.Invoke(request.DownloadInfo);
+                    }
+                    else
+                    {
+                        //_crashHandler.HandleException(e.Error);
+                    }
+                }
+                EnqueueDownload();
+            }
+        }
+        private void EnqueueDownload()
+        {
+            lock (this)
+            {
+                if (_numberOfDownloads < MaxNumberOfMultipleFileDownload)
+                {
+                    var request = EnqueueRequest();
+                    if (request != null)
+                    {
+                        Interlocked.Increment(ref _numberOfDownloads);
+                        Debug.Assert(_numberOfDownloads <= MaxNumberOfMultipleFileDownload, "This algorithm isn't thread-safe! What do you do?");
+                        request.Downloader.DownloadFileTaskAsync(request.DownloadInfo.Url, request.DownloadInfo.Path).ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+
+        private DownloadRequest EnqueueRequest()
+        {
+            return _requests.Values.FirstOrDefault(req =>
+                req.IsBusy == false && req.DownloadInfo.IsSaving &&
+                req.DownloadInfo.IsSaveComplete == false);
+        }
+
+        private void OnAddNewDownload(IDownloadInfo request)
+        {
+            AddNewDownload?.Invoke(this, request);
+        }
+
+        public List<IDownloadInfo> GetDownloadFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelAsync(IDownloadInfo downloadInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
