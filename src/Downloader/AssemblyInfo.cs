58e55beb578600491838afd41db906bebcd1b80f
Behzad <behzad.khosravifar@gmail.com>
2021-09-13 17:04:44 +0430

    improve the readability of the AssemblyInfo

--- src/Downloader/AssemblyInfo.cs
+++ src/Downloader/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.CompilerServices;
 // values defined in project properties. For details of which attributes are included
 // and how to customise this process see: https://aka.ms/assembly-info-properties
 
-
 // Setting ComVisible to false makes the types in this assembly not visible to COM
 // components.  If you need to access a type in this assembly from COM, set the ComVisible
 // attribute to true on that type.
