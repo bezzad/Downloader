82c6b32e0ab3cc5735a99cd6ab3c8ed28732242e
Behzad <behzad.khosravifar@gmail.com>
2022-01-03 00:50:39 +0330

    Refactor helper class of sample project

--- src/Downloader.Sample/Helper.cs
+++ src/Downloader.Sample/Helper.cs
@@ -4,6 +4,29 @@ namespace Downloader.Sample
 {
     public static class Helper
     {
+        public static string CalcMemoryMensurableUnit(this long bytes)
+        {
+            return CalcMemoryMensurableUnit((double)bytes);
+        }
+
+        public static string CalcMemoryMensurableUnit(this double bytes)
+        {
+            double kb = bytes / 1024; // · 1024 Bytes = 1 Kilobyte 
+            double mb = kb / 1024; // · 1024 Kilobytes = 1 Megabyte 
+            double gb = mb / 1024; // · 1024 Megabytes = 1 Gigabyte 
+            double tb = gb / 1024; // · 1024 Gigabytes = 1 Terabyte 
+
+            string result =
+                tb > 1 ? $"{tb:0.##}TB" :
+                gb > 1 ? $"{gb:0.##}GB" :
+                mb > 1 ? $"{mb:0.##}MB" :
+                kb > 1 ? $"{kb:0.##}KB" :
+                $"{bytes:0.##}B";
+
+            result = result.Replace("/", ".");
+            return result;
+        }
+
         public static void UpdateTitleInfo(this DownloadProgressChangedEventArgs e)
         {
             double nonZeroSpeed = e.BytesPerSecondSpeed + 0.0001;
@@ -23,34 +46,16 @@ namespace Downloader.Sample
                 timeLeftUnit = "unknown";
             }
 
-            string avgSpeed = CalcMemoryMensurableUnit(e.AverageBytesPerSecondSpeed);
-            string speed = CalcMemoryMensurableUnit(e.BytesPerSecondSpeed);
-            string bytesReceived = CalcMemoryMensurableUnit(e.ReceivedBytesSize);
-            string totalBytesToReceive = CalcMemoryMensurableUnit(e.TotalBytesToReceive);
+            string avgSpeed = e.AverageBytesPerSecondSpeed.CalcMemoryMensurableUnit();
+            string speed = e.BytesPerSecondSpeed.CalcMemoryMensurableUnit();
+            string bytesReceived = e.ReceivedBytesSize.CalcMemoryMensurableUnit();
+            string totalBytesToReceive = e.TotalBytesToReceive.CalcMemoryMensurableUnit();
             string progressPercentage = $"{e.ProgressPercentage:F3}".Replace("/", ".");
 
             Console.Title = $"{progressPercentage}%  -  " +
                             $"{speed}/s (avg: {avgSpeed}/s)  -  " +
                             $"{estimateTime} {timeLeftUnit} left    -  " +
                             $"[{bytesReceived} of {totalBytesToReceive}]";
-        }
-
-        public static string CalcMemoryMensurableUnit(this double bytes)
-        {
-            double kb = bytes / 1024; // · 1024 Bytes = 1 Kilobyte 
-            double mb = kb / 1024; // · 1024 Kilobytes = 1 Megabyte 
-            double gb = mb / 1024; // · 1024 Megabytes = 1 Gigabyte 
-            double tb = gb / 1024; // · 1024 Gigabytes = 1 Terabyte 
-
-            string result =
-                tb > 1 ? $"{tb:0.##}TB" :
-                gb > 1 ? $"{gb:0.##}GB" :
-                mb > 1 ? $"{mb:0.##}MB" :
-                kb > 1 ? $"{kb:0.##}KB" :
-                $"{bytes:0.##}B";
-
-            result = result.Replace("/", ".");
-            return result;
         }
     }
 }
