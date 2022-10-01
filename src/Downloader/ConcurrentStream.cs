86f314281634305bbe7e50fdd902a4903bfbdfc6
bezzad <behzad.khosravifar@gmail.com>
2022-10-01 23:55:10 +0330

    Added ConcurrentStream class with unit tests

---
+++ src/Downloader/ConcurrentStream.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Downloader
+{
+    public class ConcurrentStream : IDisposable
+    {
+        private readonly SemaphoreSlim _queueCheckerSemaphore = new SemaphoreSlim(0);
+        private readonly ManualResetEventSlim _completionEvent = new ManualResetEventSlim(true);
+        private readonly ConcurrentQueue<Packet> _inputQueue = new ConcurrentQueue<Packet>();
+        private readonly int _resourceReleaseThreshold = 10000; // packets
+        private long _packetCounter = 0;
+        private bool _disposed;
+        private Stream _stream;
+        private string _path;
+
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _path = value;
+                    _stream = new FileStream(_path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+                }
+            }
+        }
+
+        public byte[] Data
+        {
+            get
+            {
+                Flush();
+                if (_stream is MemoryStream mem)
+                    return mem.ToArray();
+
+                return null;
+            }
+            set
+            {
+                if (value != null)
+                    _stream = new MemoryStream(value, true);
+            }
+        }
+
+        public long Length => _stream?.Length ?? 0;
+
+        public ConcurrentStream(Stream stream)
+        {
+            _stream = stream;
+            Initial();
+        }
+
+        public ConcurrentStream(string filename, long initSize)
+        {
+            _path = filename;
+            _stream = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+            _stream.SetLength(initSize);
+            Initial();
+        }
+
+        public ConcurrentStream()
+        {
+            _stream = new MemoryStream();
+            Initial();
+        }
+
+        private void Initial()
+        {
+            Task.Run(Watcher).ConfigureAwait(false);
+        }
+
+        public Stream OpenRead()
+        {
+            _stream?.Seek(0, SeekOrigin.Begin);
+            return _stream;
+        }
+
+        public void WriteAsync(long position, byte[] bytes, int length)
+        {
+            _inputQueue.Enqueue(new Packet(position, bytes.Take(length).ToArray()));
+            _completionEvent.Reset();
+            _queueCheckerSemaphore.Release();
+        }
+
+        private async Task Watcher()
+        {
+            while (!_disposed)
+            {
+                await _queueCheckerSemaphore.WaitAsync().ConfigureAwait(false);
+                if (_inputQueue.TryDequeue(out var packet))
+                {
+                    await WritePacket(packet).ConfigureAwait(false);
+                    packet.Dispose();
+                    ReleasePackets();
+                }
+            }
+        }
+        private async Task WritePacket(Packet packet)
+        {
+            if (_stream.CanSeek)
+            {
+                _stream.Position = packet.Position;
+                await _stream.WriteAsync(packet.Data, 0, packet.Data.Length).ConfigureAwait(false);
+                _packetCounter++;
+            }
+
+            if (_inputQueue.IsEmpty)
+                _completionEvent.Set();
+        }
+
+        private void ReleasePackets()
+        {
+            // Clean up RAM every _resourceReleaseThreshold packet
+            if (_packetCounter % _resourceReleaseThreshold == 0)
+                GC.Collect();
+        }
+
+        public void Flush()
+        {
+            _completionEvent.Wait();
+            _stream?.Flush();
+            GC.Collect();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed == false)
+            {
+                Flush();
+                _disposed = true;
+                _queueCheckerSemaphore.Dispose();
+
+                if (_stream is FileStream fs)
+                    fs.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Downloader/Packet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Downloader
+{
+    internal struct Packet : IDisposable
+    {
+        public byte[] Data;
+        public long Position;
+
+        public Packet(long position, byte[] data)
+        {
+            Data = data;
+            Position = position;
+        }
+
+        public void Dispose()
+        {
+            Data = null;
+            Position = 0;
+        }
+    }
+}
