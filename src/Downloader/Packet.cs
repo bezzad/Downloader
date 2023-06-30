bb6947f906424497a86a6c75e52d08af79632b72
bezzad <behzad.khosravifar@gmail.com>
2023-06-30 23:43:24 +0330

    fixed packet issues

--- src/Downloader/Packet.cs
+++ src/Downloader/Packet.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 using System.Linq;
 
-namespace Downloader
+namespace Downloader;
+
+internal class Packet : IDisposable, IComparable<Packet>
 {
-    internal class Packet : IDisposable, IComparable<Packet>
+    public byte[] Data { get; set; }
+    public int Length => Data?.Length ?? 0;
+    public long Position { get; set; }
+    public long EndOffset => Position + Length;
+
+    public Packet(long position, byte[] data, int len)
     {
-        public byte[] Data { get; set; }
-        public int Length => Data.Length;
-        public long Position { get; set; }
-        public long EndOffset => Position + Length;
+        Data = data.Length > len ? data.Take(len).ToArray() : data;
+        Position = position;
+    }
 
-        public Packet(long position, byte[] data, int length)
-        {
-            Data = data.Length > length ? data.Take(length).ToArray() : Data;
-            Position = position;
-        }
+    public void Merge(Packet other)
+    {
+        Data = Data.Concat(other.Data).ToArray();
+    }
 
-        public void Merge(Packet other)
-        {
-            Data = Data.Union(other.Data).ToArray();
-        }
+    public void Dispose()
+    {
+        Data = null;
+        Position = 0;
+    }
 
-        public void Dispose()
-        {
-            Data = null;
-            Position = 0;
-        }
-
-        public int CompareTo(Packet other)
-        {
-            return Position > other.Position ? 1
-                : Position == other.Position ? 0 : -1;
-        }
+    public int CompareTo(Packet other)
+    {
+        return Position > other.Position ? 1
+            : Position == other.Position ? 0 : -1;
     }
 }
