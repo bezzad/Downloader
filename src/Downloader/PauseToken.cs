a05627ed87a7bb18ac84d2423f421387d26e5a88
bezzad <behzad.khosravifar@gmail.com>
2024-09-18 18:18:49 +0330

    convert PauseToken to record type and add xml docs

--- src/Downloader/PauseToken.cs
+++ src/Downloader/PauseToken.cs
@@ -2,16 +2,31 @@
 
 namespace Downloader;
 
-public struct PauseToken
+/// <summary>
+/// Represents a pause token that can be used to pause and resume operations.
+/// </summary>
+public record PauseToken
 {
     private readonly PauseTokenSource _tokenSource;
+
+    /// <summary>
+    /// Gets a value indicating whether the operation is paused.
+    /// </summary>
     public bool IsPaused => _tokenSource?.IsPaused == true;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PauseToken"/> class.
+    /// </summary>
+    /// <param name="source">The pause token source.</param>
     internal PauseToken(PauseTokenSource source)
     {
         _tokenSource = source;
     }
 
+    /// <summary>
+    /// Waits asynchronously while the operation is paused.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous wait operation.</returns>
     public Task WaitWhilePausedAsync()
     {
         return IsPaused
