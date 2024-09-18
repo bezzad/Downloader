c991bf1c1b90cd7888469c607009d91a9222802e
bezzad <behzad.khosravifar@gmail.com>
2024-09-18 18:23:46 +0330

    add xml doc to PauseTokenSource.cs file

--- src/Downloader/PauseTokenSource.cs
+++ src/Downloader/PauseTokenSource.cs
@@ -3,19 +3,35 @@ using System.Threading.Tasks;
 
 namespace Downloader;
 
+/// <summary>
+/// Represents a source for creating and managing pause tokens.
+/// </summary>
 public class PauseTokenSource
 {
     private volatile TaskCompletionSource<bool> _tcsPaused;
 
+    /// <summary>
+    /// Gets the pause token associated with this source.
+    /// </summary>
     public PauseToken Token => new PauseToken(this);
+
+    /// <summary>
+    /// Gets a value indicating whether the operation is paused.
+    /// </summary>
     public bool IsPaused => _tcsPaused != null;
 
+    /// <summary>
+    /// Pauses the operation by creating a new task completion source.
+    /// </summary>
     public void Pause()
     {
         // if (tcsPause == null) tcsPause = new TaskCompletionSource<bool>();
         Interlocked.CompareExchange(ref _tcsPaused, new TaskCompletionSource<bool>(), null);
     }
 
+    /// <summary>
+    /// Resumes the operation by setting the result of the task completion source and resetting it.
+    /// </summary>
     public void Resume()
     {
         // we need to do this in a standard compare-exchange loop:
@@ -38,6 +54,10 @@ public class PauseTokenSource
         }
     }
 
+    /// <summary>
+    /// Waits asynchronously while the operation is paused.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous wait operation.</returns>
     internal Task WaitWhilePausedAsync()
     {
         return _tcsPaused?.Task ?? Task.FromResult(true);
