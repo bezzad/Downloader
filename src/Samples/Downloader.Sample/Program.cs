24029baea8dc7ec4b628fb5c61d7faad7bfd84aa
Tran Ngoc Dnag Khoa <tndangkhoa218@gmail.com>
2025-11-18 21:30:16 +0700

    fix: revert Sample change

--- src/Samples/Downloader.Sample/Program.cs
+++ src/Samples/Downloader.Sample/Program.cs
@@ -8,7 +8,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -37,10 +36,7 @@ public partial class Program
             Console.Clear();
             await Initial();
             new Task(KeyboardHandler).Start();
-            
             await DownloadAll(DownloadList, CancelAllTokenSource.Token).ConfigureAwait(false);
-
-            await MultipleInvocationDownload(DownloadList.FirstOrDefault(), FileExistPolicy.Ignore, CancelAllTokenSource.Token);
         }
         catch (Exception e)
         {
@@ -148,62 +144,6 @@ public partial class Program
             await DownloadFile(downloadItem).ConfigureAwait(false);
 
             await Task.Yield();
-        }
-    }
-
-    private static async Task MultipleInvocationDownload(
-        DownloadItem downloadItem,
-        FileExistPolicy fileExistPolicy,
-        CancellationToken cancelToken)
-    {
-        if (downloadItem == null)
-        {
-            return;
-        }
-        
-        if (downloadItem.ValidateData)
-            Logger = FileLogger.Factory(downloadItem.FolderPath, Path.GetFileName(downloadItem.FileName));
-
-        CurrentDownloadConfiguration = GetDownloadConfiguration();
-        CurrentDownloadConfiguration.FileExistPolicy = fileExistPolicy;
-        CurrentDownloadService = CreateDownloadService(CurrentDownloadConfiguration, Logger);
-        
-        var fileName = string.IsNullOrEmpty(downloadItem.FileName)
-            ? new DirectoryInfo(downloadItem.FolderPath).FullName
-            : downloadItem.FileName;
-
-        var downloadTask1 = Task.Run(() => {
-            Console.WriteLine("Start thread 1");
-            CurrentDownloadService.DownloadFileTaskAsync(downloadItem.Url, fileName, cancelToken)
-                .GetAwaiter()
-                .GetResult();
-            
-            Console.WriteLine("Done thread 1");
-        }, cancelToken);
-        
-        var downloadTask2 = Task.Run(() => {
-            // Delay 3 seconds to let the first thread partially done
-            Task.Delay(10000).GetAwaiter().GetResult();
-            
-            // Redownload the file task, this is to simulate file re-initialization
-            Console.WriteLine("Start thread 2");
-            CurrentDownloadService.DownloadFileTaskAsync(downloadItem.Url, fileName, cancelToken)
-                .GetAwaiter()
-                .GetResult();
-            
-            Console.WriteLine("Done thread 2");
-        }, cancelToken);
-        
-        await Task.WhenAll(downloadTask1, downloadTask2);
-        
-        bool isValid =
-            await ValidateDataAsync(CurrentDownloadService.Package.FileName,
-                CurrentDownloadService.Package.TotalFileSize).ConfigureAwait(false);
-        if (!isValid)
-        {
-            string message = "Downloaded data is invalid: " + CurrentDownloadService.Package.FileName;
-            Logger?.LogCritical(message);
-            throw new InvalidDataException(message);
         }
     }
 
