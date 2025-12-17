2b7eaaeffff34dcaec672a065a4900f613f7ad23
bezzad <behzad.khosravifar@gmail.com>
2025-12-17 18:10:06 +0330

    refactor: simplify FolderPath property implementation in DownloadItem

--- src/Samples/Downloader.Sample/DownloadItem.cs
+++ src/Samples/Downloader.Sample/DownloadItem.cs
@@ -6,9 +6,12 @@ namespace Downloader.Sample;
 [ExcludeFromCodeCoverage]
 public class DownloadItem
 {
-    private string _folderPath;
+    public string FolderPath
+    {
+        get => field ?? Path.GetDirectoryName(FileName);
+        set;
+    }
 
-    public string FolderPath { get => _folderPath ?? Path.GetDirectoryName(FileName); set => _folderPath = value; }
     public string FileName { get; set; }
     public string Url { get; set; }
     public string[] Urls { get; set; }
