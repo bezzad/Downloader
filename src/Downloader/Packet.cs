d8905a22f3a8153e6e2294ef29c54f27fd539eaa
bezzad <behzad.khosravifar@gmail.com>
2026-01-24 13:01:56 +0330

    fixed Packet.cs class to exposes only the valid slice without copying.

--- src/Downloader/Packet.cs
+++ src/Downloader/Packet.cs
@@ -4,7 +4,17 @@ namespace Downloader;
 
 internal class Packet(long position, ReadOnlyMemory<byte> data, int len) : IDisposable, ISizeableObject
 {
-    public ReadOnlyMemory<byte> Data { get; private set; } = data[..len];
+    /// <summary>
+    /// Exposes only the valid slice without copying.
+    /// Note: Please don't use `data[..len]` instead of this code, because it has a performance issue.
+    /// </summary>
+    public ReadOnlyMemory<byte> Data = data.Slice(0, len);
+
+    /// <summary>
+    /// Fast path for consumers that can work with spans.
+    /// </summary>
+    public ReadOnlySpan<byte> Span => Data.Span;
+    
     public int Length { get; } = len;
     public readonly long Position = position;
     public readonly long EndOffset = position + len;
